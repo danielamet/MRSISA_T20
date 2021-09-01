c373b925680f1205a4b6800f8054409ad18c492e
milos012 <milos441@gmail.com>
2021-09-01 02:53:42 +0200

    Dodata stranica za kreiranje nove apoteke

---
+++ frontend/src/components/AddPharmacyComponent.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import { Paper } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import Checkbox from '@material-ui/core/Checkbox';
+import axios from 'axios';
+
+const paperStyle={padding: "30px 50px",width:370,margin:"20px auto"}
+const avatarStyle={backgroundColor: "#1bbd7e"}
+const headerStyle={margin:0}
+const marginTop={marginTop:5}
+
+
+function AlertSucc() {
+  const [open, setOpen] = React.useState(true);
+  return (
+    //Popup.alert('You pressed the Save btn')
+    <Alert
+    >
+      Close me!
+    </Alert>
+  );
+}
+
+
+const AddPharmacy=()=>{
+
+
+    return(
+        <Grid>
+            <Paper elevation={20} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}>
+                        <AddIcon/>
+                    </Avatar>
+                    <h2 style={headerStyle}>Add new pharmacy</h2>
+                </Grid>
+                <form>
+                    <TextField
+                        name="name"
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="name"
+                        label="Name"
+                        autoFocus
+                        style={marginTop}
+                    />
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="address"
+                        label="Address"
+                        name="address"
+                        style={marginTop}
+                    />
+                    <TextField
+                        name="description"
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="description"
+                        label="Description"
+                        autoFocus
+                        style={marginTop}
+                    />
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    onClick="AlertSucc()"
+                    >
+                        submit
+                    </Button>
+                </form>
+            </Paper>
+        </Grid>
+    );
+}
+
+export default AddPharmacy;
