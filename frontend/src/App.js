13055cd595fa1b7074273d5aa01e6fc3211626b1
milos012 <milos441@gmail.com>
2021-09-01 23:33:53 +0200

    dodat ruter i basic homepage sa navigacijom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import AddMedicine from './components/AddMedicineComponent';
+import AddPharmacy from './components/AddPharmacyComponent';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <AddMedicine/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+        {/* <AddMedicine/> */}
+        {/* <AddPharmacy/> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Isa Mrs</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/addMed">New Medicine</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
